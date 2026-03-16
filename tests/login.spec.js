6a417d672c16ca59a31335ef7b17048eab244a7f
Anuj Jadhav <aj.jadhav2001@gmail.com>
2026-03-16 12:59:26 +0530

    updated the env variables and added smoke tag in test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,9 +1,9 @@
 const {test, expect} = require('../fixtures/baseTest.js');
 require('dotenv').config();
 
-test('Login test @smoke', async ({page, loginPage}) => {
+test('@smoke Login test', async ({page, loginPage}) => {
     await page.goto(process.env.BASE_URL);
-    console.log(process.env.USER, process.env.PASSWORD);
-    await loginPage.login(process.env.USER, process.env.PASSWORD);
+    console.log(process.env.SAUCE_USER, process.env.SAUCE_PASSWORD);
+    await loginPage.login(process.env.SAUCE_USER, process.env.SAUCE_PASSWORD);
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
 })
