4fe3f58834c5eb7ea645e7ce9cd333a7897cee06
Anuj Jadhav <aj.jadhav2001@gmail.com>
2026-03-16 14:56:21 +0530

    updated the yml file to chnage worker nodes

--- playwright.config.js
+++ playwright.config.js
@@ -17,11 +17,11 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: !! process.env.CI,
   /* Retry on CI only */
   retries: 2,
   /* Opt out of parallel tests on CI. */
-  workers: 3,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
